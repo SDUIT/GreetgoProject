6c4b9588b04c1b22ebc113cccf206d73c0438eaf
sduit <zohirjonkabirov@gmail.com>
2018-01-28 11:52:20 +0600

    eight Commit

--- app/src/main/java/com/example/zohir/greetgoproject/Search_activity.java
+++ app/src/main/java/com/example/zohir/greetgoproject/Search_activity.java
@@ -8,6 +8,8 @@ import android.net.wifi.WifiManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,14 +18,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Search_activity extends AppCompatActivity {
 
-    private TextView t;
+    public TextView t;
 
+    public String work;
+    private String s;
     private WifiManager wifiManager;
     private List<ScanResult> wifiList;
+
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,37 @@ public class Search_activity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         t = (TextView) findViewById(R.id.ip);
-        String s = getIntent().getStringExtra("Ip");
-        t.setText(s);
+        s = getIntent().getStringExtra("Ip");
+        t.setText( s );
+//        work = (Spannable) t.getText();
+
+        new Thread(){
+            public void run(){
+              while(!isInterrupted()) {
+                  System.out.println("Thread Running");
+                  try {
+
+                      detectWifi();
+                      runOnUiThread(new Runnable() {
+                          @Override
+                          public void run() {
+                              if( flag == false ) {
+                                  t.setText("He/She not in wife Zone");
+                              }
+                              else {
+                                  t.setText(work);
+                              }
+                          }
+                      });
+                      Thread.sleep(1000);
+
+                  }
+                  catch (InterruptedException e) {
+                      e.printStackTrace();
+                  }
+              }
+            }
+        }.start();
 
     }
     public void detectWifi() {
@@ -45,7 +81,7 @@ public class Search_activity extends AppCompatActivity {
 
         Log.d("TAG", wifiList.toString());
 
-        boolean flag = false;
+         flag = false;
         for( int i=0; i<wifiList.size(); i++ ) {
 
             String item = wifiList.get(i).toString();
@@ -56,20 +92,33 @@ public class Search_activity extends AppCompatActivity {
             String item_ssid = vector_item[0];
             String item_capabilities = vector_item[1];
             String item_level = vector_item[3];
+            String item_frequency = vector_item[4];
             String ssid = item_ssid.split(": ")[1];
             String security = item_capabilities.split(": ")[1];
             String level = item_level.split(": ")[1];
+            String frequency = item_frequency.split(": ")[1];
 
-            if ( item_capabilities.equals(t) ) {
+            System.out.println( security );
+
+            double ans  = calculateDistance(Double.parseDouble(level), Double.parseDouble(frequency));
+            if ( security.equals(s) ) {
                 flag = true;
-                t.setText( ssid );
+                work = String.valueOf(ans);
+//              work = new SpannableString("He is in: " + String.valueOf(ans));
+                break;
             }
         }
         if( flag == false ) {
-            t.setText("He is not in wifi Zone");
+//            work = new SpannableString("He is not in wifi Zone");
         }
 
     }
+
+    public double calculateDistance(double signalLevelInDb, double freqInMHz) {
+        double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(signalLevelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
